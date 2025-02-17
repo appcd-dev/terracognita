65c30b680ce4734b628fcb90a6ebb22619c4b1ec
Nasir Rabbani <nasir@appcd.com>
2025-02-17 21:07:35 +0530

    use region filter upfront in ListBuckets call

--- aws/reader/list_buckets.go
+++ aws/reader/list_buckets.go
@@ -2,22 +2,25 @@ package reader
 
 import (
 	"context"
-	"errors"
-	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	s3types "github.com/aws/aws-sdk-go-v2/service/s3/types"
 )
 
 func (c *connector) ListBuckets(ctx context.Context, input *s3.ListBucketsInput) ([]s3types.Bucket, error) {
-	var errs []error
-	var ropt = &s3.ListBucketsOutput{}
-
 	if c.svc.s3 == nil {
 		c.svc.s3 = s3.NewFromConfig(c.svc.config)
 	}
 
 	opt := s3.ListBucketsOutput{}
+
+	if input == nil {
+		input = &s3.ListBucketsInput{}
+	}
+
+	if c.svc.region != "" {
+		input.BucketRegion = &c.svc.region
+	}
 
 	hasNextToken := true
 	for hasNextToken {
@@ -39,31 +42,5 @@ func (c *connector) ListBuckets(ctx context.Context, input *s3.ListBucketsInput)
 		opt.Buckets = append(opt.Buckets, o.Buckets...)
 	}
 
-	newOpt := &s3.ListBucketsOutput{
-		Owner:   opt.Owner,
-		Buckets: make([]s3types.Bucket, 0),
-	}
-	for _, bucket := range opt.Buckets {
-		inputLocation := &s3.GetBucketLocationInput{
-			Bucket: bucket.Name,
-		}
-		result, err := c.svc.s3.GetBucketLocation(ctx, inputLocation)
-		if err != nil {
-			errs = append(errs, err)
-		}
-		if string(result.LocationConstraint) == c.svc.region {
-			newOpt.Buckets = append(newOpt.Buckets, bucket)
-		}
-	}
-	ropt = newOpt
-
-	if len(errs) != 0 {
-		serrs := make([]string, 0, len(errs))
-		for _, e := range errs {
-			serrs = append(serrs, e.Error())
-		}
-		return nil, errors.New(strings.Join(serrs, ","))
-	}
-
-	return ropt.Buckets, nil
+	return opt.Buckets, nil
 }
